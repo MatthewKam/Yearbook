23d35f737ebc191e85b6ca4a5accdc63e08f915b
Matt Kam <kamaeleo@makbook.home>
2014-06-12 00:51:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yearbook::Application.config.secret_key_base = 'd45e00c52d8711ea2b79042cb9cb123c329c4753207934e1060fcc414fcd712ff386fc06bbaf6a3b39470df8dc6bb3ceebcf2e7f981d2ab3349ca593ea182d48'
