3be9d15a5f00ae68a5e0af7a03a41950e402af63
Matt Kam <kamaeleo@makbook.home>
2014-06-17 01:09:21 -0700

    link_to update and video update

--- app/assets/javascripts/yearbook.js
+++ app/assets/javascripts/yearbook.js
@@ -1,7 +1,7 @@
 var yearbookApp = angular.module('yearbook_app', []);
 
 yearbookApp.controller('YearbookCtrl', ['$scope', function($scope) {
-    $scope.videoUrl = ["/assets/lounge0.mp4", "/assets/lounge1.mp4", "/assets/lounge2.mp4", "/assets/lounge3.mp4", "/assets/lounge4.mp4"];
+    $scope.videoUrl = ["/assets/lounge1.mp4", "/assets/lounge2.mp4", "/assets/lounge3.mp4", "/assets/lounge4.mp4"];
     $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())]; 
     $scope.changeVideo = function(){
       $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())];
