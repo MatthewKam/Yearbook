8ad616fd0c57ada074ebcd36f923ce3d650635d6
Matt Kam <kamaeleo@makbook.att.net>
2014-06-21 20:39:26 -0700

    scrolling landing page

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,36 @@
+	
+  $(document).ready(function(){
+    $('a[href="#wdihome"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
+
+
+
+  $(document).ready(function(){
+    $('a[href="#classphotohome"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
