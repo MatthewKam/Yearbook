59792fedca855662dcbdddbb2ac22f00f76a24eb
Matt Kam <kamaeleo@maKbook.local>
2014-06-27 17:17:52 -0700

    video changes

--- app/assets/javascripts/yearbook.js
+++ app/assets/javascripts/yearbook.js
@@ -1,12 +1,38 @@
 var yearbookApp = angular.module('yearbook_app', []);
 
+// yearbookApp.controller('YearbookCtrl', ['$scope', function($scope) {
+//     $scope.videoUrl = ["/assets/lounge1.mp4", "/assets/lounge2.mp4", "/assets/lounge3.mp4", "/assets/lounge4.mp4", "/assets/lounge5.mp4", "/assets/lounge6.mp4", "/assets/lounge7.mp4","/assets/lounge8.mp4"];
+//     $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())]; 
+//     $scope.changeVideo = function(){
+//       $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())];
+//     };
 yearbookApp.controller('YearbookCtrl', ['$scope', function($scope) {
     $scope.videoUrl = ["/assets/lounge1.mp4", "/assets/lounge2.mp4", "/assets/lounge3.mp4", "/assets/lounge4.mp4", "/assets/lounge5.mp4", "/assets/lounge6.mp4", "/assets/lounge7.mp4","/assets/lounge8.mp4"];
-    $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())]; 
+    $scope.randomIndex = Math.floor($scope.videoUrl.length*Math.random());
+    $scope.randomVideo = $scope.videoUrl[$scope.randomIndex];
     $scope.changeVideo = function(){
-      $scope.randomVideo = $scope.videoUrl[Math.floor($scope.videoUrl.length*Math.random())];
+    	$scope.randomIndex = Math.floor($scope.videoUrl.length*Math.random());
+      $scope.randomVideo = $scope.videoUrl[$scope.randomIndex];
     };
-
+    document.getElementById('bgvid').addEventListener('ended',playNext,false);
+    function playNext(e) {
+        if(!e) { e = window.event; }
+					if ($scope.randomIndex == $scope.videoUrl.length - 1) {
+						console.log("you have hit the end");
+						$scope.randomIndex = 0;
+						$scope.$apply(function() {
+							$scope.randomVideo = $scope.videoUrl[$scope.randomIndex];
+							document.getElementById('bgvid').play();
+						});
+					} else {
+						console.log('Incrementing');
+						$scope.randomIndex++;
+						$scope.$apply(function() {
+							$scope.randomVideo = $scope.videoUrl[$scope.randomIndex];
+							document.getElementById('bgvid').play();
+						});
+					}
+    }
 
 
     $scope.students = [
